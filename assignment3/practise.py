f1a81983024ad238519272d23e28f26c1eec7205
ชลธิชา สีมันตะ <s5902041620075@Macmini-213-24.local>
2019-07-11 16:14:04 +0700

    update for practise.py

---
+++ assignment3/practise.py
@@ -0,0 +1,9 @@
+my_list =  [25, 25, 50]
+print(sum(my_list)) 
+
+my_tuple = (2, 3, 5, 6)
+print(my_tuple)
+
+set1 = {1, 5, 9, 12, 15, 18, 77, 88}
+set2 = {3, 5, 7, 9, 12, 20, 77, 100}
+print(set1.intersection(set2))

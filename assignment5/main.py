fd9b1c2e1151e3577a9395a9df0c63e8da012bc7
Computer Education <computereducation@Macmini-213-24.local>
2019-08-01 14:40:24 +0700

    add Modify assignment5

--- assignment5/main.py
+++ assignment5/main.py
@@ -1,31 +1,9 @@
-from tkinter import *
+from flask import Flask,render_template
+app = Flask(__name__)
 
-root =Tk()
+@app.route('/')
+def index():
+    return render_template('Register.html')
 
-l1=Label(root,text='name:')
-l2=Label(root,text='surname:')
-l3=Label(root,text='username: ')
-l4=Label(root,text='email: ')
-l5=Label(root,text='password')
-
-t1=Entry(root,textvariable=StringVar())
-t2=Entry(root,textvariable=StringVar())
-t3=Entry(root,textvariable=StringVar())
-t4=Entry(root,textvariable=StringVar())
-t5=Entry(root,show='*' ,textvariable=StringVar())
-
-b1=Button(root,text='Login')
-
-l1.pack()
-t1.pack()
-l2.pack()
-t2.pack()
-l3.pack()
-t3.pack()
-l4.pack()
-t4.pack()
-l5.pack()
-t5.pack()
-b1.pack()
-
-root.mainloop()
+if __name__ == '__main__':
+    app.run()

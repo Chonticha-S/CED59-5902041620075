0c6fe0f18cbfa500e4a44c6cfa652f093d4d2432
Computer Education <computereducation@Macmini-213-24.local>
2019-08-08 16:24:42 +0700

    update assignment7

--- assignment7/main.py
+++ assignment7/main.py
@@ -1,9 +1,18 @@
 from flask import Flask,render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('Register.html')
+    return render_template('login.html')
+
+@app.route('/',methods=['post'])
+def register():
+    return 'OK'
 
 if __name__ == '__main__':
     app.run()
+

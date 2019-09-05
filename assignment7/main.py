ed82817508fae831bdf2c69fe7ddbab1d57ad5ec
Computer Education <computereducation@Macmini-213-24.local>
2019-09-05 14:25:08 +0700

    Add assignmene8

--- assignment7/main.py
+++ assignment7/main.py
@@ -1,7 +1,4 @@
 from flask import Flask,render_template
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
@@ -11,7 +8,7 @@ def index():
 
 @app.route('/',methods=['post'])
 def register():
-    return 'OK'
+    return 'Chonticha'
 
 if __name__ == '__main__':
     app.run()
